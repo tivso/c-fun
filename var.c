b3c73291d14edbcf129b0ebd756b8d3b2e4cf3ff
tivso <ollershiba@email.com>
2026-03-15 10:05:40 +0800

    first day programs i made in c

---
+++ var.c
@@ -0,0 +1,37 @@
+#include "stdio.h"
+#include <stdbool.h>
+
+int main(){
+
+  int a = 1;
+
+  printf("a= %d\n", a);
+
+  float gpa = 2.67;
+
+    printf("your gpa was %f u dumbass\n", gpa);
+
+  double pi =  2.312234;
+
+  printf("why are u incorrect in pi why is it %lf you stupid noob\n", pi);
+
+  char l = 'A';
+
+  printf("anyways you got a %c\n", l);
+
+  char w[] = "jaiden";
+
+    printf("so your name was %s\n", w);
+
+ bool Isstudent = true;
+
+  if(Isstudent){
+    printf("ur a student\n");
+  }
+ else {
+    printf("your not a student\f");
+ 
+
+    return 0;
+}
+}
---
+++ variables.c
@@ -0,0 +1,39 @@
+#include "stdio.h"
+#include <stdbool.h>
+
+
+int main(){
+
+  int a = 2;
+
+  printf("you got a A and A is equal to %d in this school\n", a);
+
+  float gpa = 2.27;
+
+  printf("because of that your gpa is %f you should try better next time\n", gpa);
+
+  double pi = 6.212312321;
+
+  printf("hey your pi is wrong loser dumb why is it %lf loser gay\n", pi);
+
+  char name[] = "joben";
+
+  printf("so ur name is %s stupid gay name\n", name);
+
+  bool IsGay = false;
+
+    if (IsGay) {
+       printf("you are gay");
+    }
+  else {
+    printf("your are not gay\n");
+  }
+
+  return 0;
+
+    
+
+
+
+
+}
